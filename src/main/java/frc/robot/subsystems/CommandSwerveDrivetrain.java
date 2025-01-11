65c679c0dca5029c6852f097d4444197730ebeb3
MrBotBoy <zebotboy@gmail.com>
2025-01-10 23:49:28 -1000

    updated Pathplanner

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -22,6 +22,7 @@ import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -254,6 +255,8 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
                 angleToTurnTo = (findClosestApriltag(Constants.apriltagPosesXYRedCoral).getRotation().getDegrees() + 180.0) % 180;
             }
         }
+
+        SmartDashboard.putNumber("turnToWhatAngle", angleToTurnTo);
     }
 
     private void startSimThread() {
