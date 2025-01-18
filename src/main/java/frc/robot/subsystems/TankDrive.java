aca20872f928a271a3be068db610df5c69fac22b
MrBotBoy <zebotboy@gmail.com>
2025-01-17 17:29:57 -1000

    update kitbot

--- src/main/java/frc/robot/subsystems/TankDrive.java
+++ src/main/java/frc/robot/subsystems/TankDrive.java
@@ -1,10 +1,19 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TankDrive extends SubsystemBase{
+
+    SparkMax lSparkMax;
+    SparkMax rSparkMax;
     
     public TankDrive() {
-        
+
+    }
+
+    public void setDrivePower(double leftSide, double rightSide) {
+
     }
 }
