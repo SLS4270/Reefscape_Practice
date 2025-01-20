5356b6f7b7ca2c7e3ef746367f8e7d2307b84957
MrBotBoy <zebotboy@gmail.com>
2025-01-19 17:27:04 -1000

    update arm intake

--- src/main/java/frc/robot/subsystems/ArmIntake.java
+++ src/main/java/frc/robot/subsystems/ArmIntake.java
@@ -1,17 +1,27 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmIntake extends SubsystemBase{
-    TalonFX armIntake;
+    SparkMax armIntake;
+    CANrange rangeFinder;
 
     public ArmIntake() {
-
+        armIntake = new SparkMax(0, MotorType.kBrushless);
+        rangeFinder = new CANrange(40);
     }
 
     public void spinArmIntake(double power) {
+
+    }
+
+    @Override
+    public void periodic() {
         
     }
 }
